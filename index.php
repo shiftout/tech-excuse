f9bae1178b51b71573c8ed52c2078dcd8abc22a9
shiftout <shiftout@protonmail.com>
2018-05-15 17:16:09 +0200

    add some CSS

--- index.php
+++ index.php
@@ -6,30 +6,36 @@ include_once("./functions.php");
 <head>
   <meta charset="UTF-8">
   <title>Tech Excuse Generator</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <h1>What?</h1>
-  <p>Blame Technology! Find an excuse or submit your own!</p>
+  <div id="container">
+    <h1>Tech Excuse Generator</h1>
+    <p>Blame Technology! Find an excuse or submit your own!</p>
 
-  <h3>Random excuse</h3>
-  <?php
-    $excuse = (isset($_POST['excuse_id'])) ? random_excuse($_POST['excuse_id']) : random_excuse();
-    print htmlentities($excuse['desc'] . "(".$excuse['id'].")");
-  ?>
-  <form action="index.php" method="POST">
-    <input type="hidden" name="excuse_id" value="<?php print $excuse['id']; ?>"/>
-    <input type="submit" value="Refresh">
-  </form>
+    <div class="section">
+      <h2>Random excuse</h2>
+      <?php
+        $excuse = (isset($_POST['excuse_id'])) ? random_excuse($_POST['excuse_id']) : random_excuse();
+        print "<pre>" . htmlentities($excuse['desc']) . "</pre>";
+      ?>
+      <form action="index.php" method="POST">
+        <input type="hidden" name="excuse_id" value="<?php print $excuse['id']; ?>"/>
+        <input type="submit" value="Refresh">
+      </form>
+    </div>
 
-
-  <h3>Any inputs?</h3>
-  <?php if (isset($_POST['excuse'])) {
-    add_excuse($_POST['excuse']);
-  }?>
-  <form action="index.php" method="POST">
-    <label for="excuse">Excuse</label>
-    <input type="text" name="excuse">
-    <input type="submit" value="Submit">
-  </form>
+    <div class="section">
+      <h2>Your input</h2>
+      <?php if (isset($_POST['excuse'])) {
+        add_excuse($_POST['excuse']);
+      }?>
+      <form action="index.php" method="POST">
+        <label for="excuse">What's your excuse?</label>
+        <input type="text" name="excuse">
+        <input type="submit" value="Submit">
+      </form>
+    </div>
+  </div>
 </body>
 </html>
